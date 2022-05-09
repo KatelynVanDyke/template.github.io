fc71322c3575f7b59a2e4be257d04070c7d6d2cc
Marimbagirl2020 <Marimbagirl2020@gmail.com>
2022-05-09 18:55:44 -0500

    Upload Files

---
+++ script/script.js
@@ -0,0 +1,15 @@
+/* Website Template © Katelyn Van Dyke 2022 */
+
+/* Menu */
+
+function openMenu() {
+    document.getElementById("nav-mobile-menu").style.display = "none";
+    document.getElementById("nav-mobile-close").style.display = "block";
+    document.getElementById("nav-mobile-links").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("nav-mobile-menu").style.display = "block";
+    document.getElementById("nav-mobile-close").style.display = "none";
+    document.getElementById("nav-mobile-links").style.display = "none";
+}
